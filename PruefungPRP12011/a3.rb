5d3c7769d4fe52f00708ef4f1750b3e568a11f42
Nhat Khanh Huy Tran <khanhhuy288@gmail.com>
2017-07-10 22:39:28 +0200

    almost finish PT2011 A3

--- PruefungPRP12011/a3.rb
+++ PruefungPRP12011/a3.rb
@@ -16,7 +16,7 @@ module Mitarbeiter
   end
 
   def jahres_gehalt
-    @gehalt + bonus
+    @gehalt * 12.0 + bonus
   end
 
   def bonus
@@ -51,19 +51,31 @@ class Manager
   include Mitarbeiter
   def initialize(name, nr, gehalt)
     super(name, nr, gehalt)
-    @personen = []
+    @mas = []  
   end
   
   def << ma
-    @personen << ma
+    @mas << ma
   end
 
   def bonus
-    
+    @mas.inject(0) { |sum, ma| sum + ma.bonus } / mas_num * 1.8
+  end
+  
+  def mas_num 
+    result = 0 
+    @mas.each { |ma|
+      result += 1
+      if ma.is_a?(Manager)
+        result += ma.mas_num
+      end
+    }
+    puts result
+    result
   end
   
   def berichts_ebene
-     
+    @mas.max { |a, b| a.berichts_ebene <=> b.berichts_ebene }.berichts_ebene + 1
   end
 
   def to_s()
